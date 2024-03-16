b0a2f429907a0fa3c74615a799baca7d0f4612a6
Mack <clarson9@uncc.edu>
2024-03-16 15:35:36 -0400

    Added Scene for testing Character, Added healthbar and global variables

---
+++ Assets/CharacterTestingScripts/GlobalScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalScript : MonoBehaviour
+{
+    public static int health;
+    public static int maxHealth;
+
+        
+}
---
+++ Assets/CharacterTestingScripts/PlayerControllerTesting.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTesting : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 direction;
+    public float speed = 8;
+    public float jumpForce = 10;
+    public float gravity = -20;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    Quaternion nonzeroWalkRotation;
+    public Transform cameraRotator;
+    public Transform cameraRotatorDummy;
+
+    public bool ableToMakeADoubleJump = true;
+
+    public Transform meshTransform;
+
+    private Transform GetCameraRotation()
+    {
+        cameraRotatorDummy.eulerAngles = cameraRotator.eulerAngles;
+        cameraRotatorDummy.eulerAngles = new Vector3(0, cameraRotator.eulerAngles.y, 0);
+        return cameraRotatorDummy;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlobalScript.health = 100;
+        GlobalScript.maxHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        direction.x = 0;
+        direction.z = 0;
+
+        if (isGrounded)
+        {
+            direction.y = -1;
+            ableToMakeADoubleJump = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                direction.y = jumpForce;
+            }
+        }
+        else
+        {
+            direction.y += gravity * Time.deltaTime;
+            if (ableToMakeADoubleJump && Input.GetButtonDown("Jump"))
+            {
+                direction.y = jumpForce;
+                ableToMakeADoubleJump = false;
+            }
+        }
+        controller.Move(direction * Time.deltaTime);
+
+        float runMultiplier = (Input.GetKey("right shift")) ? 2f : 1f;
+
+        Vector3 walkDirection = new Vector3();
+        //walkDirection.x = hInput * speed;
+        //walkDirection.z = vInput * speed;
+        Transform currentCameraRot = GetCameraRotation();
+        walkDirection += currentCameraRot.right * hInput * speed * runMultiplier;
+        walkDirection += currentCameraRot.forward * vInput * speed * runMultiplier;
+        controller.Move(walkDirection * Time.deltaTime);
+
+        if (walkDirection.magnitude != 0)
+            nonzeroWalkRotation = meshTransform.localRotation;
+
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+        meshTransform.localRotation = Quaternion.LookRotation(walkDirection);
+
+        if (walkDirection.magnitude == 0)
+            meshTransform.localRotation = nonzeroWalkRotation;
+
+        //isGrounded = Physics.CheckSphere(groundCheck.position, 0.09f, groundLayer);
+        //isGrounded = Physics.CheckSphere(groundCheck.position, 0.5f, groundLayer);
+
+        isGrounded = Physics.CheckCapsule(new Vector3(groundCheck.position.x, groundCheck.position.y, groundCheck.position.z),
+            new Vector3(groundCheck.position.x, groundCheck.position.y - 0.09f, groundCheck.position.z), 0.5f, groundLayer);
+    }
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = isGrounded ? Color.green : Color.red;
+    //    Gizmos.DrawWireSphere(groundCheck.position, 0.09f);
+    //}
+}
