1d68d57cdc0fe7864478bba8048d589d469a5242
FatalCutie <jcfrosh360@gmail.com>
2024-04-15 21:45:18 -0400

    Rewrote Towers
    Towers now have a toggle for range, redid all prefabs to be in line with this change. Added gattling tower.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -11,6 +11,9 @@ public class Tower : MonoBehaviour
     public float range = 15f;
     public float baseFireRate = 1f; //fire once a second by default
     private float fireCountdown = 0f; //is divided by firerate
+    public enum Mode {Close, Distant}
+    public Mode fireMode;
+    public float angleOffset = 90;
 
     [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
@@ -25,7 +28,7 @@ public class Tower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("UpdateTarget", 0f, 0.5f); //Update target every half second
+        InvokeRepeating("UpdateTarget", 0f, baseFireRate/2); //Update target every half second
     }
 
     void UpdateTarget()
@@ -38,26 +41,53 @@ public class Tower : MonoBehaviour
 
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
-        GameObject nearestEnemy = null;
+        float farthestDistance = 0;
+        GameObject closestEnemy = null;
+        GameObject farthestEnemy = null;
+
         foreach (GameObject enemy in enemies) //find the nearest enemy
         {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if (distanceToEnemy < shortestDistance)
+            if (fireMode == Mode.Distant)
             {
-                shortestDistance = distanceToEnemy;
-                nearestEnemy = enemy;
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy > farthestDistance && distanceToEnemy <= range)
+                {
+                    farthestDistance = distanceToEnemy;
+                    farthestEnemy = enemy;
+                }
+            } else if(fireMode == Mode.Close)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    closestEnemy = enemy;
+                }
             }
+            
 
         }
 
-        if (nearestEnemy != null && shortestDistance <= range)
+        if(fireMode == Mode.Close)
         {
-            //print("Switching target!");
-            target = nearestEnemy.transform; //switch target to nearest enemy
-        } else {
-            target = null;
-            //print("There are no enemies to target!");
+            if (closestEnemy != null && shortestDistance <= range)
+            {
+                target = closestEnemy.transform; //switch target to nearest enemy
+            } else {
+                target = null;
+            }
+        }else if(fireMode == Mode.Distant)
+        {
+            if (farthestEnemy != null && farthestDistance <= range)
+            {
+                //print("Switching target!");
+                target = farthestEnemy.transform; //switch target to nearest enemy
+            } else {
+                target = null;
+                //print("There are no enemies to target!");
+            }
         }
+        
         
     }
 
@@ -74,7 +104,7 @@ public class Tower : MonoBehaviour
          Vector3 dir = target.position - transform.position;
          Quaternion lookRotation = Quaternion.LookRotation(dir);
          Vector3 rotation = lookRotation.eulerAngles;
-         partToRotate.rotation = Quaternion.Euler (0f, rotation.y + 90f, 0f); //-180 is an offset for clockwork tower
+         partToRotate.rotation = Quaternion.Euler (0f, rotation.y + angleOffset, 0f); //-180 is an offset for clockwork tower
 
         if (fireCountdown <= 0f)
         {
